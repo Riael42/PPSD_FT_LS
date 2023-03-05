a7724a62310449c5b94d237595a0f2b4c75ab1dc
Riael <pagabrie@student.42berlin.de>
2023-03-05 13:52:46 +0100

    Starting work on arg processor

--- srcs/ft_proc_args.c
+++ srcs/ft_proc_args.c
@@ -1,22 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_proc_args.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: riael <Don't quit! | #42gether>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/05 12:23:23 by riael             #+#    #+#             */
-/*   Updated: 2023/03/05 13:46:40 by riael            ###   ########.fr       */
+/*   Created: 2023/03/05 13:44:49 by riael             #+#    #+#             */
+/*   Updated: 2023/03/05 13:52:25 by riael            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "../includes/ft_ls.h"
 
-int	main(int argc, char **argv)
+void	ft_proc_args(int argc, char **argv)
 {
-	argv[0][0] += 1;
-	if (argc == 1)
-		write(1, "Running default LS config\n", 26);
-	else
-		ft_proc_args(argc, argv);
-	return (0);
+	int	num;
+
+	num = 1;
+	while (num < argc)
+	{
+		if (argv[num][0] == '-') //if argv[num][1] == '-' check advanced flags for errors
+			if (ft_check_flag(argv[num]) == 0)
+			{
+				write(1, "Error not a valid flag", 21);
+				return ;
+			}
+		num += 1;
+	}
 }
